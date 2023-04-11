8e1c4267fa216449120180445806cdb736143c1e
KinaraGeorge <54885174+KinaraGeorge@users.noreply.github.com>
2023-04-12 00:08:37 +0300

    Update 8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,2 +1,15 @@
 #!/usr/bin/node
-
+const arg = [];
+const { argv } = require('process');
+const square = 'X';
+let x;
+argv.forEach((val, index) => {
+  arg[index] = `${val}`;
+});
+const number = Number(`${arg[2]}`);
+if (isNaN(number)) {
+  console.log('Missing size');
+} else {
+  for (x = 0; x < number; x++) {
+    console.log(square.repeat(number));
+  }
